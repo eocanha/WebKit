81fa5789b359b4307d2ba1b90c4557282886c952
Enrique Ocaña González <eocanha@igalia.com>
2024-05-14 21:05:41 +0200

    [DEBUG] Prevent a second play() and any pause()
    ...when this env var is defined:
    
    export DEBUG_ONLY_ALLOW_FIRST_PLAY=1

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -414,6 +414,23 @@ bool MediaPlayerPrivateGStreamer::isPipelineSeeking() const
 
 void MediaPlayerPrivateGStreamer::play()
 {
+    static bool onlyAllowFirstPlay = false;
+    static std::once_flag once;
+    std::call_once(once, []() {
+        auto s = String::fromLatin1(std::getenv("DEBUG_ONLY_ALLOW_FIRST_PLAY"));
+        if (!s.isEmpty()) {
+            String value = s.stripWhiteSpace().convertToLowercaseWithoutLocale();
+            onlyAllowFirstPlay = (value == "1"_s || value == "t"_s || value == "true"_s);
+        }
+    });
+
+    static int timesCalled = 0;
+    timesCalled++;
+    if (onlyAllowFirstPlay && timesCalled > 1) {
+        GST_INFO("!!!! REFUSING to play() a %dth time FOR DEBUGGING PURPOSES !!!!", timesCalled);
+        return;
+    }
+
     if (isMediaStreamPlayer()) {
         m_pausedTime = MediaTime::invalidTime();
         if (m_startTime.isInvalid())
@@ -443,6 +460,23 @@ void MediaPlayerPrivateGStreamer::play()
 
 void MediaPlayerPrivateGStreamer::pause()
 {
+    static bool onlyAllowFirstPlay = false;
+    static std::once_flag once;
+    std::call_once(once, []() {
+        auto s = String::fromLatin1(std::getenv("DEBUG_ONLY_ALLOW_FIRST_PLAY"));
+        if (!s.isEmpty()) {
+            String value = s.stripWhiteSpace().convertToLowercaseWithoutLocale();
+            onlyAllowFirstPlay = (value == "1"_s || value == "t"_s || value == "true"_s);
+        }
+    });
+
+    static int timesCalled = 0;
+    timesCalled++;
+    if (onlyAllowFirstPlay && timesCalled > 0) {
+        GST_INFO("!!!! REFUSING to pause() a %dth time FOR DEBUGGING PURPOSES !!!!", timesCalled);
+        return;
+    }
+
     if (isMediaStreamPlayer())
         m_pausedTime = currentMediaTime();
 
