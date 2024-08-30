a04444221acf6522fa058b44fd2910fc037461f6
Enrique Ocaña González <eocanha@igalia.com>
2024-08-30 14:51:03 +0200

    [FIX] Fix layout test
    http/tests/media/video-play-stall-seek.html
    
    Need a short description (OOPS!).
    Need the bug URL (OOPS!).
    
    Reviewed by NOBODY (OOPS!).
    
    Explanation of why this fixes the bug (OOPS!).
    
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::updateBufferingStatus):
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -150,6 +150,8 @@ GST_DEBUG_CATEGORY(webkit_media_player_debug);
 namespace WebCore {
 using namespace std;
 
+bool isMediaDiskCacheDisabled();
+
 static const FloatSize s_holePunchDefaultFrameSize(1280, 720);
 
 static void initializeDebugCategory()
@@ -2298,6 +2300,11 @@ void MediaPlayerPrivateGStreamer::updateBufferingStatus(GstBufferingMode mode, d
 
     if (m_didDownloadFinish)
         m_fillTimer.stop();
+    else {
+        bool shouldDownload = !m_isLiveStream.value_or(false) && m_preload == MediaPlayer::Preload::Auto && !isMediaDiskCacheDisabled();
+        if (shouldDownload)
+            m_fillTimer.startRepeating(200_ms);
+    }
 
     m_bufferingPercentage = percentage;
 
